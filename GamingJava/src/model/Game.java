802837498beebcbb8c9052fc2b391d61becbd885
lamecode <kislaya.sliva@gmail.com>
2021-06-30 17:08:55 +0300

    Adding getters and setters

--- GamingJava/src/model/Game.java
+++ GamingJava/src/model/Game.java
@@ -4,14 +4,45 @@ import java.util.Random;
 
 public class Game {
 
+    private int minValue;
+    private int maxValue;
     private int randomNumber;
     private int[] statistics;
 
     public Game() {
         randomNumber = new Random().nextInt(101);
         statistics = new int[0];
+        minValue = Boundaries.MIN_VALUE;
+        maxValue = Boundaries.MAX_VALUE;
     }
 
+    public int getMaxValue() {
+        return maxValue;
+    }
 
+    public void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void appendToStatistics(int stat) {
+        int[] newStat = new int[statistics.length + 1];
+        for (int i = 0; i < statistics.length; i++) {
+            newStat[i] = statistics[i];
+        }
+        newStat[statistics.length] = stat;
+        this.statistics = newStat;
+    }
 
 }
