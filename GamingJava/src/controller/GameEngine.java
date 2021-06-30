bbb20f58d703bf066851f546f11ef096c2e44916
lamecode <kislaya.sliva@gmail.com>
2021-06-30 19:28:10 +0300

    Checking user input

--- GamingJava/src/controller/GameEngine.java
+++ GamingJava/src/controller/GameEngine.java
@@ -8,16 +8,47 @@ public class GameEngine {
 
     Game game;
     Input input;
-    ProgramView programView;
+    ProgramView view;
 
     public GameEngine() {
         game = new Game();
         input = new Input();
-        programView = new ProgramView();
+        view = new ProgramView();
     }
 
     public void start() {
+        view.printMessage(view.OPTION_INPUT,
+                String.valueOf(game.getMinValue()),
+                String.valueOf(game.getMaxValue()));
+        input.writeUserInput();
+        retypeInput();
+    }
 
+    private String validate(String input) {
+        if (!isNumeric(input)) {
+            return view.STRING_INPUT;
+        } else if (Integer.parseInt(input) < game.getMinValue()
+                || Integer.parseInt(input) > game.getMaxValue()){
+            return view.INPUT_OUT_OF_RANGE;
+        } else {
+            return "";
+        }
+    }
+
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private void retypeInput() {
+        String validatorVal = validate(input.getUserInput());
+        while (true) {
+            if (validatorVal.equals("")) {
+                break;
+            } else {
+                view.printError(validatorVal);
+                input.writeUserInput();
+            }
+        }
     }
 
 }
