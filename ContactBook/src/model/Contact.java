af733fd353720b0f6b49bd07b10d0adfc1f4cf97
lamecode <kislaya.sliva@gmail.com>
2021-07-04 02:28:58 +0300

    full name constructor

--- ContactBook/src/model/Contact.java
+++ ContactBook/src/model/Contact.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Contact {
 
     private FullName fullName;
-    private String initials;
     private String nickname;
     private String comment;
     private Group group;
@@ -14,6 +16,15 @@ public class Contact {
 
     public Contact(FullName fullName, String nickname, String comment,
                    Group group, Contacts contacts, Address address) {
-        
+        this.fullName = fullName;
+        this.nickname = nickname;
+        this.comment = comment;
+        this.group = group;
+        this.contacts = contacts;
+        this.address = address;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        this.dateCreated = dtf.format(now);
+        this.dateModified = dateCreated;
     }
 }
--- ContactBook/src/model/FullName.java
+++ ContactBook/src/model/FullName.java
@@ -1,7 +1,20 @@
 package model;
 
 public class FullName {
+
     private String name;
     private String surname;
     private String patronimic;
+    private String initials;
+
+    FullName(String name, String surname, String patronimic) {
+        this.name = name;
+        this.surname = surname;
+        this.patronimic = patronimic;
+        this.initials = createInitials();
+    }
+
+    private String createInitials() {
+        return surname + " " + name.toCharArray()[0] + ".";
+    }
 }
