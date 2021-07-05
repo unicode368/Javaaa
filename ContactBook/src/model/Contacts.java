2728ded246e72c93be1c69ba6f246fc59f5bb3b2
lamecode <kislaya.sliva@gmail.com>
2021-07-05 12:11:51 +0300

    creating Contacts constructor

--- ContactBook/src/model/Contacts.java
+++ ContactBook/src/model/Contacts.java
@@ -6,4 +6,13 @@ public class Contacts {
     private String mobilePhone2;
     private String email;
     private String skype;
+
+    public Contacts(String homePhone, String mobilePhone,
+                    String mobilePhone2, String email, String skype) {
+        this.homePhone = homePhone;
+        this.mobilePhone = mobilePhone;
+        this.mobilePhone2 = mobilePhone2;
+        this.email = email;
+        this.skype = skype;
+    }
 }
