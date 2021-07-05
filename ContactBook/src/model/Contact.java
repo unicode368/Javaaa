6a38c248f12194e8daab8deb83b5b7f738b5b5de
lamecode <kislaya.sliva@gmail.com>
2021-07-05 12:11:33 +0300

    creating constructor with empty fields

--- ContactBook/src/model/Contact.java
+++ ContactBook/src/model/Contact.java
@@ -16,6 +16,18 @@ public class Contact {
     private final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private final LocalDateTime NOW = LocalDateTime.now();
 
+    public Contact(String nickname, String comment,
+                   Group group, Contacts contacts) {
+        this.fullName = new FullName("", "", "");
+        this.nickname = nickname;
+        this.comment = comment;
+        this.group = group;
+        this.contacts = contacts;
+        this.address = new Address(0, "", "", 0, 0);
+        this.dateCreated = DTF.format(NOW);
+        this.dateModified = dateCreated;
+    }
+
     public Contact(FullName fullName, String nickname, String comment,
                    Group group, Contacts contacts, Address address) {
         this.fullName = fullName;
@@ -26,5 +38,13 @@ public class Contact {
         this.address = address;
         this.dateCreated = DTF.format(NOW);
         this.dateModified = dateCreated;
+    }
+
+    public Contacts createContacts(String homePhone,
+                                   String mobilePhone,
+                                   String mobilePhone2,
+                                   String email,
+                                   String skype) {
+        return new Contacts(homePhone, mobilePhone, mobilePhone2, email, skype);
     }
 }
