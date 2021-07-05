6c1b7ef8070f60d6a2ca08fb833b507ea9b81f2c
lamecode <kislaya.sliva@gmail.com>
2021-07-05 12:12:20 +0300

    input for FullName

--- ContactBook/src/controller/Controller.java
+++ ContactBook/src/controller/Controller.java
@@ -1,21 +1,45 @@
 package controller;
 
+import view.Input;
+import view.ProgramView;
+
 public class Controller {
 
-    public Controller() {
+    private Input input;
+    private ProgramView view;
 
+    public Controller() {
+        input = new Input();
+        view = new ProgramView();
     }
 
     public void start() {
-
+        String name = takeInput(view.NAME_INPUT, "ім'я");
+        String surname = takeInput(view.SURNAME_INPUT, "прізвище");
+        String patronimic = takeInput(view.PATRONIMIC_INPUT, "по-батькові");
+        String nickname = takeInput(view.NICKNAME_INPUT, "прізвище");
     }
 
-    public void takeInput() {
-
+    public String takeInput(String message, String type) {
+        view.printMessage(message);
+        input.writeUserInput();
+        while (!validate(input.getUserInput())) {
+            printByType(type);
+            input.writeUserInput();
+        }
+        return input.getUserInput();
     }
 
-    public void validate() {
+    public void printByType(String type) {
+        if (type.equals("група")) {
+            view.printError(view.WRONG_GROUP);
+        } else {
+            view.printError(view.WRONG_INPUT, type);
+        }
+    }
 
+    public boolean validate(String input) {
+        return false;
     }
 
 }
