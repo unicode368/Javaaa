b00069f5323710f33a3a29d064a3a09e5262a097
lamecode <kislaya.sliva@gmail.com>
2021-07-05 18:05:48 +0300

    new regex pattern

--- ContactBook/src/model/RegexPatterns.java
+++ ContactBook/src/model/RegexPatterns.java
@@ -3,6 +3,6 @@ package model;
 public class RegexPatterns {
 
     public final String NAME_REGEX = "^[A-Z][a-z]{1,}$";
-    public final String NICKNAME_REGEX = ""
+    public final String NICKNAME_REGEX = "^.{2,}$";
 
 }
