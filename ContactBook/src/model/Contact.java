9ea02afc246d0c07aa164a53ade7d59d05bffaa0
lamecode <kislaya.sliva@gmail.com>
2021-07-05 15:47:33 +0300

    defining some fields of Contact with methods

--- ContactBook/src/model/Contact.java
+++ ContactBook/src/model/Contact.java
@@ -16,19 +16,18 @@ public class Contact {
     private final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private final LocalDateTime NOW = LocalDateTime.now();
 
-    public Contact(String nickname, String comment,
-                   Group group) {
+    public Contact(String nickname, String comment) {
         this.fullName = new FullName("", "", "");
         this.nickname = nickname;
         this.comment = comment;
-        this.group = group;
+        this.group = null;
         this.contacts = new Contacts("", "", "", "", "");
         this.address = new Address(0, "", "", 0, 0);
         this.dateCreated = DTF.format(NOW);
         this.dateModified = dateCreated;
     }
 
-    public Contact(FullName fullName, String nickname, String comment,
+    Contact(FullName fullName, String nickname, String comment,
                    Group group, Contacts contacts, Address address) {
         this.fullName = fullName;
         this.nickname = nickname;
@@ -40,11 +39,46 @@ public class Contact {
         this.dateModified = dateCreated;
     }
 
-    public Contacts createContacts(String homePhone,
-                                   String mobilePhone,
-                                   String mobilePhone2,
-                                   String email,
-                                   String skype) {
-        return new Contacts(homePhone, mobilePhone, mobilePhone2, email, skype);
+    public void defineContacts(String homePhone, String mobilePhone,
+                                   String mobilePhone2, String email, String skype) {
+        setContacts(new Contacts(homePhone, mobilePhone, mobilePhone2, email, skype));
+    }
+
+    public void defineAddress(int index, String city, String street,
+                                   int houseNumber, int flatNumber) {
+        setAddress(new Address(index, city, street, houseNumber, flatNumber));
+    }
+
+    public void defineFullName(String surname, String name,
+                                   String patronimic) {
+        FullName fullName = new FullName(surname, name, patronimic);
+        setFullName(fullName);
+    }
+
+    public void defineGroup(int i) {
+        switch (i) {
+            case 1: setGroup(Group.FAMILY);
+                break;
+            case 2: setGroup(Group.WORK);
+                break;
+            case 3: setGroup(Group.FRIENDS);
+                break;
+        }
+    }
+
+    private void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    private void setGroup(Group group) {
+        this.group = group;
+    }
+
+    private void setContacts(Contacts contacts) {
+        this.contacts = contacts;
+    }
+
+    private void setAddress(Address address) {
+        this.address = address;
     }
 }
