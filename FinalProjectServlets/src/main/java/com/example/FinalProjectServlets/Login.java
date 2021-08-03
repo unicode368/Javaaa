cd6e01bc28fb049dd4e417cbc80a3a84de0f2327
lamecode <kislaya.sliva@gmail.com>
2021-08-03 22:40:01 +0300

    creating servlet project with html template

---
+++ FinalProjectServlets/src/main/java/com/example/FinalProjectServlets/Login.java
@@ -0,0 +1,35 @@
+package com.example.FinalProjectServlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "login", value = "/login")
+public class Login extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req,
+                          HttpServletResponse res) throws IOException {
+        PrintWriter pw=res.getWriter();
+        res.setContentType("text/html");
+        String user=req.getParameter("Username");
+        String pass=req.getParameter("Password");
+        pw.println("Login Success...!");
+        if(user.equals("Sobaka") && pass.equals("Sobaka"))
+            pw.println("Login Success...!");
+        else
+            pw.println("Login Failed...!");
+        pw.close();
+    }
+
+    public void doGet(HttpServletRequest request,  HttpServletResponse response) {
+        try{
+            response.sendRedirect("login.html");
+        } catch (Exception exp){
+            System.out.println(exp);
+        }
+    }
+}
