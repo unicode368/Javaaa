d322dd866d2143d4b5234e1dd7daa7349de30c0a
lamecode <kislaya.sliva@gmail.com>
2021-08-12 16:25:33 +0300

    user dto

--- FinalProjectSpring/src/main/java/com/ua/faculty/dto/UserDTO.java
+++ FinalProjectSpring/src/main/java/com/ua/faculty/dto/UserDTO.java
@@ -2,13 +2,44 @@ package com.ua.faculty.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @ToString
+
 public class UserDTO {
-    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String login;
+
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String patronimic;
+
+    @NotNull
+    @NotEmpty
     private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String phoneNumber;
 }
