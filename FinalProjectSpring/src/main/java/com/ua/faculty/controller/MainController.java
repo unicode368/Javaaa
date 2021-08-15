138ab2ee48e0dc834a9dfae601a3c8f597af2c9c
lamecode <kislaya.sliva@gmail.com>
2021-08-16 01:03:46 +0300

    processing courses

--- FinalProjectSpring/src/main/java/com/ua/faculty/controller/MainController.java
+++ FinalProjectSpring/src/main/java/com/ua/faculty/controller/MainController.java
@@ -1,14 +1,24 @@
 package com.ua.faculty.controller;
 
+import com.ua.faculty.entity.Course;
+import com.ua.faculty.repository.CourseRepository;
+import com.ua.faculty.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private CourseRepository courseRepository;
+
    @RequestMapping(value = "/", method = RequestMethod.GET)
-   public String index() {
+   public String index(Model model) {
+       Iterable<Course> courses = courseRepository.findAll();
+       model.addAttribute("courses", courses);
        return "index";
    }
 
