9d4f79d73cb25c9e96ae09c8695b93fc2b9dbd60
lamecode <kislaya.sliva@gmail.com>
2021-09-09 12:23:33 +0300

    adding sorting method

--- FinalProjectSpring/src/main/java/com/ua/faculty/service/CourseService.java
+++ FinalProjectSpring/src/main/java/com/ua/faculty/service/CourseService.java
@@ -1,10 +1,58 @@
 package com.ua.faculty.service;
 
+import com.ua.faculty.entity.Course;
+import com.ua.faculty.repository.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 @AllArgsConstructor
 public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public List<Course> sort(List<Course> courses, String sortType) {
+        switch (sortType) {
+            case "nameASC":
+                courses.sort(
+                        Comparator.comparing(Course::getName,
+                                Comparator.naturalOrder()));
+                break;
+            case "nameDESC":
+                courses.sort(
+                        Comparator.comparing(Course::getName,
+                                Comparator.reverseOrder()));
+                break;
+            case "duration":
+                courses.sort(
+                        Comparator.comparing(o ->
+                                getDuration(o.getId())));
+                break;
+            case "studentAmount":
+                courses.sort(
+                        Comparator.comparing(o ->
+                                o.getStudents().size()));
+                break;
+        }
+        return courses;
+    }
+
+    public Long getDuration(Long id) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+        return DAYS.between(course.getEndDate(),
+                course.getStartDate());
+    }
+
+
 
 }
