1c42dce0aba6193d19e89cc390f7c3c8686bb072
lamecode <kislaya.sliva@gmail.com>
2021-09-09 23:59:29 +0300

    RequestMapping for create teacher and blocking user

--- FinalProjectSpring/src/main/java/com/ua/faculty/controller/AdminController.java
+++ FinalProjectSpring/src/main/java/com/ua/faculty/controller/AdminController.java
@@ -7,8 +7,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.websocket.server.PathParam;
 import java.util.Collections;
 
 @Controller
@@ -18,9 +22,18 @@ public class AdminController {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
-    @RequestMapping("/admin/create-teacher")
-    public String createTeacher(Model model) {
+    @RequestMapping(value = "/admin/create-teacher",
+            method = RequestMethod.GET)
+    public String showCreateTeacherForm(Model model) {
         return "create_teacher";
+    }
+
+    @RequestMapping(value = "/admin/create-teacher",
+            method = RequestMethod.POST)
+    public String createTeacher(Model model) {
+        model.addAttribute("message",
+                "Teacher created successfully");
+        return "redirect:success";
     }
 
     @RequestMapping("/admin/users")
@@ -33,5 +46,12 @@ public class AdminController {
         return "students";
     }
 
+    @RequestMapping(value = "/admin/user/{id}", method = RequestMethod.POST)
+    public String changeBlockStatus(@PathParam(value = "id") final Integer id,
+                                    Model model) {
+        model.addAttribute("message",
+                "Teacher created successfully");
+        return "redirect:success";
+    }
 
 }
